eafb1360114c6686f2f927921d25d70efe00ef31
coreyareid <coreyareid@bellsouth.net>
2022-07-07 16:09:18 -0500

    Complted exercise

---
+++ Syntax_Sugar_Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using Syntax_Sugar_Exercise;
+//Exercise 1
+
+SyntaxExercise.Syntax_Exercise();
+
+// Below was the original code.
+
+/*
+int answer = 4;
+string response;
+if (answer < 9)
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}
+*/
---
+++ Syntax_Sugar_Exercise/SyntaxExercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Syntax_Sugar_Exercise
+{
+    internal class SyntaxExercise
+    {
+       public static void Syntax_Exercise()
+
+        {
+            int answer = 4;
+
+            string response = (answer < 9)
+                ? answer + " is less than nine "
+                : answer + " is greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
+
